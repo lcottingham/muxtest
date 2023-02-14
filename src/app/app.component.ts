2d86a9107a75e53b591603b41c9ca0d961653ed5
Luke Cottingham <lcottingham@users.noreply.github.com>
2023-02-14 22:12:21 +0000

    Mux added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import "@mux/mux-player";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mux-test';
+  playbackId = "FuJSYrK0014ec2LPnm11bzC2MAySAQPqA";
 }
